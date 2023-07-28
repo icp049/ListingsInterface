c101fdf27ac2686435485cef05b721d031383ca2
icp049 <ianpepe2000@gmail.com>
2023-07-28 03:15:23 -0600

    removed userchats for now

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -44,7 +44,7 @@ const Register = () => {
             });
 
             //create empty user chats on firestore
-            await setDoc(doc(db, "userChats", res.user.uid), {});
+            
             navigate("/");
           } catch (err) {
             console.log(err);
