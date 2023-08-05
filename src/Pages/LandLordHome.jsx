def5407139d73645a2a3c7c084bcbbee33ee71fd
icp049 <ianpepe2000@gmail.com>
2023-08-04 23:39:28 -0600

    add listing button

--- src/Pages/LandLordHome.jsx
+++ src/Pages/LandLordHome.jsx
@@ -30,7 +30,7 @@ const LandLordHome = () => {
   return (
     <div>
         <Link to = "/addlisting">
-      <button> add Listing </button>
+      <button> add Listing sdfsd </button>
       </Link>
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
         <h1>{getGreetingMessage()}</h1>
